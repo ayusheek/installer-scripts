693e2583b072f55d2d449090c6c4756d6fe1f5e0
ayusheek <ayushkr91997@gmail.com>
2024-11-15 03:09:52 +0530

    Update aquatone.sh

--- aquatone.sh
+++ aquatone.sh
@@ -1,6 +1,6 @@
 wget "https://dl.google.com/linux/direct/google-chrome-canary_current_amd64.deb"
 sudo apt install "./google-chrome-canary_current_amd64.deb" -y
-rm google-chrome-canary_current_amd64.deb
+rm "google-chrome-canary_current_amd64.deb"
 wget "https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip"
 unzip "aquatone_linux_amd64_1.7.0.zip"
-rm LICENSE.txt README.md aquatone_linux_amd64_1.7.0.zip
+rm "LICENSE.txt" "README.md" "aquatone_linux_amd64_1.7.0.zip"
